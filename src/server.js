dc3ff552fff9969579cb8b41fedd3c8b3755fc83
Mircea M <mircea.moise@gmail.com>
2020-09-07 19:40:40 +0100

    [Mircea]: Updating the connection details so that it matches to what MHS is expecting

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from './app';
 import logger from './config/logging';
 import { initialiseSubscriber } from './services/queue/subscriber';
 
-initialiseSubscriber();
+initialiseSubscriber({ durable: false, exclusive: false, 'auto-delete': false });
 
 app.listen(3000, () => logger.info('Listening on port 3000'));
