c33428f29d5b4e22b02d87f04e192caf165064d3
Tomasz Setkowski <tomasz.setkowski2@nhs.net>
2020-12-14 10:32:45 +0000

    Don't start subscriber listening on the queue

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from './app';
 import { initialiseSubscriber } from './services/queue/subscriber';
 import { logEvent } from './middleware/logging';
 
-initialiseSubscriber();
+// initialiseSubscriber();
 
 app.listen(3000, () => logEvent('Listening on port 3000'));
