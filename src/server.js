ca57a78172900bbf9ffef25e80451ae303303ed6
Tomasz Setkowski <tomasz.setkowski2@nhs.net>
2020-12-14 10:29:47 +0000

    Don't start http listener. Only poll the queue

--- src/server.js
+++ src/server.js
@@ -4,4 +4,9 @@ import { logEvent } from './middleware/logging';
 
 initialiseSubscriber();
 
-app.listen(3000, () => logEvent('Listening on port 3000'));
+// app.listen(3000, () => logEvent('Listening on port 3000'));
+console.log('Worker is started. Press any key to exit');
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.on('data', process.exit.bind(process, 0));
